202edb9e84db10884995a2f5465d9fc6f248b102
Jacob Jude <jacobj.jude@outlook.com>
2022-09-22 11:29:48 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            IEnumerable<string> LFruits = from fruit in fruits 
+            where fruit.StartsWith("L")
+            select fruit;
+
+            Console.WriteLine("Fruits that start with 'L'");
+            foreach(string fruit in LFruits)
+            {
+                Console.WriteLine($"{fruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Numbers that are multiples of 4 or 6");
+            foreach(int num in fourSixMultiples)
+            {
+                Console.WriteLine($"{num}");
+            }
+        }
+    }
+}
